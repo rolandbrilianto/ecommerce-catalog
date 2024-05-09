397de114b6f9afd984c0417e73733e9e298bdc34
Roland Brilianto <rolandbrilianto@gmail.com>
2024-05-10 06:32:00 +0700

    feat(UI): progress ui 50%

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="background"
+    :class="{'bgMale':gender==='male','bgFemale':gender === 'female'}">
+      <div class="container">
+        <div class="productBox">
+
+          <div class="gambar">
+            gambar
+          </div>
+
+          <div class="deskripsi">
+            <div>judul</div>
+            <div>kategori</div>
+            <div>isi</div>
+            <div>harga</div>
+            <div>tombol</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      gender:'female'
+    }
   }
 }
 </script>
 
 <style>
+body{
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+  
+}
+.background{
+  height: 66vh;
+  
+}
+
+.bgFemale{
+  background-color: #FDE2FF;
+  
+}
+
+.bgMale{
+  background-color: #D6E6FF;
+  
+}
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  height: 100%;
+}
+.productBox{
+  display: flex;
+  flex-direction: row;
+  top: 15%;
+  position: absolute;
+  width: 80%;
+  height: 70%;
+  box-shadow: 2px 4px 21px 0 rgba(0, 0, 0, 0.2);
+  background-color: white;
+  border-radius: 10px;
+}
+.gambar{
+  flex-basis: 33.333333%
+}
+.deskripsi{
+  flex-basis: 66.666667%;
+  display: flex;
+  flex-direction: column;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
