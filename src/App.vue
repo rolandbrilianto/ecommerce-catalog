85daba4c822655c4e7180dd7ebda693283a4318d
Roland Brilianto <rolandbrilianto@gmail.com>
2024-05-10 22:15:44 +0700

    fix(core): ui done 100%

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app">
-    <div class="background"
-    :class="{'bgMale':gender==='male','bgFemale':gender === 'female'}">
+    <div :class="['background', gender === 'male' ? 'bgMan' : 'bgWoman']">
       <div class="container">
         <div class="productBox">
-
           <div class="gambar">
-            gambar
+            <img src="@/assets/ImgTest.png" class="fotoGambar" alt="gambar" />
           </div>
 
           <div class="deskripsi">
-            <div>judul</div>
-            <div>kategori</div>
-            <div>isi</div>
-            <div>harga</div>
-            <div>tombol</div>
+            <div :class="[gender === 'female' ? 'judulWoman' : 'judulMan']">
+              Lock and Love Women's Removable Hooded Faux Leather Moto Biker
+              Jacket
+            </div>
+            <div class="kategori">
+              <p style="color: #3f3f3f">Pakaian</p>
+              <div :class="[gender === 'female' ? 'ratingWoman' : 'ratingMan']">
+                <div class="nilai">2.4/5</div>
+                <div v-for="i in 5" :key="i" :class="circleClasses(i)"></div>
+              </div>
+            </div>
+            <div class="horizontal-line"></div>
+            <p style="margin-bottom: 35px">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sequi
+              reiciendis illum ipsum pariatur officiis amet cumque corporis modi
+              repellat. Mollitia ipsa veritatis cumque ea aspernatur, rem
+              voluptatem dignissimos accusantium.
+            </p>
+            <div class="horizontal-line"></div>
+            <div :class="[gender === 'female' ? 'hargaWoman' : 'hargaMan']">
+              $29.5
+            </div>
+            <div class="tombol">
+              <button
+                :class="[
+                  gender === 'female' ? 'tombolFillWoman' : 'tombolFillMan',
+                ]"
+              >
+                Buy Now
+              </button>
+              <button
+                :class="[
+                  gender === 'female' ? 'tombolBorderWoman' : 'tombolBorderMan',
+                ]"
+              >
+                Next Product
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -24,43 +55,56 @@
 
 <script>
 export default {
-  data(){
-    return{
-      gender:'female'
-    }
-  }
-}
+  data() {
+    return {
+      gender: "male",
+      productRating: 4,
+    };
+  },
+  methods: {
+    circleClasses(i) {
+      return {
+        circleWoman: this.gender === "female",
+        circleMan: this.gender === "male",
+        filledWoman: this.gender === "female" && i <= this.productRating,
+        filledMan: this.gender === "male" && i <= this.productRating,
+        borderWoman: this.gender === "female",
+        borderMan: this.gender === "male",
+      };
+    },
+  },
+};
 </script>
 
 <style>
-body{
-    margin: 0;
-    padding: 0;
-    height: 100vh;
-  
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
+html {
+  font-family: "Inter", sans-serif;
 }
-.background{
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
+.background {
   height: 66vh;
-  
+  background-image: url("@/assets/bg-pattern.svg");
 }
 
-.bgFemale{
-  background-color: #FDE2FF;
-  
+.bgWoman {
+  background-color: #fde2ff;
 }
 
-.bgMale{
-  background-color: #D6E6FF;
-  
+.bgMan {
+  background-color: #d6e6ff;
 }
-.container{
+.container {
   display: flex;
   justify-content: center;
   align-items: center;
-  
   height: 100%;
 }
-.productBox{
+.productBox {
   display: flex;
   flex-direction: row;
   top: 15%;
@@ -71,15 +115,126 @@ body{
   background-color: white;
   border-radius: 10px;
 }
-.gambar{
-  flex-basis: 33.333333%
+.gambar {
+  flex-basis: 33.333333%;
 }
-.deskripsi{
+.fotoGambar {
+  width: 250px;
+  margin-top: 50px;
+  margin-left: 50px;
+}
+.deskripsi {
   flex-basis: 66.666667%;
   display: flex;
   flex-direction: column;
+  margin-right: 50px;
+}
+.judulWoman,
+.judulMan {
+  font-size: 30px;
+  font-weight: 600;
+  margin-top: 25px;
+}
+.judulWoman {
+  color: #720060;
+}
+.judulMan {
+  color: #002772;
+}
+.kategori {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.ratingWoman,
+.ratingMan {
+  display: flex;
+  margin-top: auto;
+  margin-bottom: auto;
+}
+.nilai {
+  margin-right: 4px;
+}
+.circleWoman,
+.circleMan {
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  background-color: white;
+  margin-right: 3px;
+  border: 1px solid transparent;
+}
+.borderWoman {
+  border-color: #720060;
+}
+.borderMan {
+  border-color: #002772;
+}
+.filledWoman {
+  background-color: #720060;
+}
+.filledMan {
+  background-color: #002772;
+}
+.horizontal-line {
+  border-top: 1px solid #000;
+  opacity: 20%;
+}
+.hargaWoman,
+.hargaMan {
+  font-weight: 600;
+  font-size: 30px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.hargaWoman {
+  color: #720060;
+}
+.hargaMan {
+  color: #002772;
+}
+.tombol {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.tombolFillWoman,
+.tombolFillMan {
+  flex-basis: 49%;
+  height: 42px;
+  color: white;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 4px;
+  border: none;
+}
+.tombolFillWoman {
+  background-color: #720060;
+}
+.tombolFillMan {
+  background-color: #002772;
+}
+.tombolBorderWoman,
+.tombolBorderMan {
+  flex-basis: 49%;
+  height: 42px;
+  background-color: transparent;
+  border: solid 3px;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 4px;
+}
+.tombolBorderWoman {
+  color: #720060;
+  border-color: #720060;
+}
+.tombolBorderMan {
+  color: #002772;
+  border-color: #002772;
 }
 #app {
- 
 }
 </style>
